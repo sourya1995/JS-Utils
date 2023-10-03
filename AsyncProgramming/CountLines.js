1560b7fb67a494f247d9cf0dce0ed3b9eb701509
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-10-03 12:32:16 +0530

    Adding any() and all() to Futures class

---
+++ AsyncProgramming/CountLines.js
@@ -0,0 +1,14 @@
+import fs from 'fs-extra-promise';
+
+const filename = process.argv[2];
+
+fs.readFileAsync(filename, { encoding: 'utf8' })
+.then(data => {
+    const length = data.split('\n').length - 1;
+})
+.catch(err => {
+    console.error(err.message)
+});
+
+/*
+* UTF-8 specifies how characters are stored as bytes
--- AsyncProgramming/Future.js
+++ AsyncProgramming/Future.js
@@ -1,27 +1,32 @@
+/*
+* this is based on something called Fluent Interface, which allows us to chain methods together
+*
+*/
+
 class Future {
     constructor(action) {
         this.actionCallbacks = [];
-        this.errorCallback = () => {};
+        this.errorCallback = () => { };
         action(this.onResolve.bind(this), this.onReject.bind(this));
     }
 
-    then(thenHandler){
+    then(thenHandler) {
         this.actionCallbacks.push(thenHandler);
         return this;
     }
 
-    catch(errorHandler){
+    catch(errorHandler) {
         this.errorCallback = errorHandler;
         return this;
     }
 
-    onResolve(value){
+    onResolve(value) {
         let storedValue = value;
-        try{
+        try {
             this.actionCallbacks.forEach((action) => {
                 storedValue = action(storedValue);
-            }); 
-        } catch(error) {
+            });
+        } catch (error) {
             this.actionCallbacks = [];
             this.onReject(err);
         }
@@ -30,6 +35,39 @@ class Future {
     onReject(err) {
         this.errorCallback(err);
     }
+
+    static any(futures) {
+        return new Future((onResolve, onReject) => {
+            futures.forEach(future => {
+                future.then((value) => {
+                    onResolve(value);
+                }).catch((error) => {
+                    console.error(error.message);
+                })
+            })
+        })
+    }
+
+    static all(futures) {
+        return new Future((onResolve, onReject) => {
+            const results = new Array(futures.length);
+            let resolvedCount = 0;
+
+            futures.forEach((future, index) => {
+                future.then((value) => {
+                    results[index] = value;
+                    resolvedCount++;
+
+                    if (resolvedCount === futures.length) {
+                        onResolve(results);
+                    }
+                }).catch((error) => {
+                    onReject(error);
+                })
+            })
+        })
+    }
 }
 
 export default Future;
+
