96a67dd7b7ed89aa75073e88984c92f1132e9e2e
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2024-02-09 11:36:47 +0530

    constructor pattern

---
+++ DesignPatterns/Constructor/constructor.js
@@ -0,0 +1,10 @@
+class Shape {
+    constructor(color, sides, name){
+        this.color = color;
+        this.sides = sides;
+        this.name = name;
+    }
+}
+
+var shape = new Shape("blue", "4", "square");
+console.log(shape);
