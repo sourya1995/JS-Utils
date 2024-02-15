9a560435d08b0a246766c99e88f9fdb753971541
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2024-02-15 16:40:03 +0530

    Bridge pattern

---
+++ DesignPatterns/Creational/Bridge/Bridge.js
@@ -0,0 +1,71 @@
+class Applications {
+    constructor(name, type){
+        this.name = name;
+        this.type = type;
+    }
+    display() {}
+    displayMode() {}
+}
+
+class Mode {
+    constructor(app){
+        this.app = app;
+        this.lightMode = function(){
+            this.app.setLightMode()
+        }
+        this.darkMode = function(){
+            this.app.setDarkMode()
+        }
+    }
+}
+
+class Facebook extends Applications {
+    constructor(name, type){
+        super(name, type)
+        this.mode =  "light";
+        this.setLightMode = function(){
+            this.mode = "light";
+        }
+        this.setDarkMode = function(){
+            this.mode = "dark";
+        }
+       
+    }
+    display(){
+        console.log(`Welcome to facebook for ${this.type}.`);
+    }
+
+    displayMode(){
+        console.log(`You are using facebook in ${this.mode} mode.`);
+    }
+}
+
+class WhatsApp extends Applications {
+    constructor(name, type){
+        super(name, type)
+        this.mode =  "light";
+        this.setLightMode = function(){
+            this.mode = "light";
+        }
+        this.setDarkMode = function(){
+            this.mode = "dark";
+        }
+    }
+    display(){
+        console.log(`Welcome to WhatsApp for ${this.type}.`);
+    }
+
+    displayMode(){
+        console.log(`You are using WhatsApp in ${this.mode} mode.`);
+    }
+}
+
+const fb = new Facebook("Facebook", "Social Network")
+const mode = new Mode(fb)
+mode.darkMode()
+fb.displayMode()
+
+const whatsapp = new WhatsApp("Whatsapp", "Chatting")
+const mode2 = new Mode(whatsapp)
+mode2.lightMode()
+whatsapp.displayMode()
